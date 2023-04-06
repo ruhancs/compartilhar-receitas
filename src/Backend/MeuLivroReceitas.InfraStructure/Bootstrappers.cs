7cb146ea5d8b283eb46fc7ae4a220d0ce1c9ab51
Ruhan Correa Soares <ruhan_cs@hotmail.com>
2023-04-06 13:53:13 -0300

    Adicionando classes de migraçao da tabela de usuarios

---
+++ src/Backend/MeuLivroReceitas.InfraStructure/Bootstrappers.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using FluentMigrator.Runner;
+using MeuLivroReceitas.Domain.Extension;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MeuLivroReceitas.InfraStructure;
+
+//Classe para replicar builder.Service
+//extension de builder.Service
+//utilizada para injetar dependencias
+public static class Bootstrappers
+{
+    public static void AddRepository(this IServiceCollection service, IConfiguration configManager)
+    {
+        AddFluentMigrator(service, configManager);
+    }
+
+    private static void AddFluentMigrator(IServiceCollection service, IConfiguration configManager)
+    {
+        var connectionString = configManager.GetFullConnectionString();
+        
+        service.AddFluentMigratorCore().ConfigureRunner(c => 
+        c.AddMySql5()
+        .WithGlobalConnectionString(connectionString).ScanIn(Assembly.Load("MeuLivroReceitas.InfraStructure"))
+        .For.All()//procurar em todo MeuLivroReceitas.InfraStructure classes para migrar
+        );
+    }
+}
---
+++ src/Backend/MeuLivroReceitas.InfraStructure/Migrations/EnumVersions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuLivroReceitas.InfraStructure.Migrations;
+
+public enum EnumVersions
+{
+    CreateUserTable = 1
+}
