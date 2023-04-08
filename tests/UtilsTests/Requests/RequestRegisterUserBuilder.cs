ed21b2926d691dcd4a61dc5edad6d2065f9beb6c
Ruhan Correa Soares <ruhan_cs@hotmail.com>
2023-04-08 18:31:23 -0300

    criado testes dos validator de registrar usuario

---
+++ tests/UtilsTests/Requests/RequestRegisterUserBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+using MeuLivroReceitas.Comunication.Request;
+
+namespace UtilsTests.Requests;
+
+public class RequestRegisterUserBuilder
+{
+    public static RequestRegisterUserJson Construct(int passwordLength = 10)
+    {
+        //Faker vem de Bogus
+        return new Faker<RequestRegisterUserJson>()
+            .RuleFor(c => c.Name, f => f.Person.FullName)//gerar um nome
+            .RuleFor(c => c.Email, f => f.Internet.Email())//gerar um email
+            .RuleFor(c => c.Password, f => f.Internet.Password(passwordLength))//gerar um senha
+            .RuleFor(c => c.Phone, f => f.Phone.PhoneNumber("## ! ####-####").Replace("!", $"{f.Random.Int(min:1, max:9)}"));//gerar uma password no formato
+            
+    }
+}
---
+++ tests/Validators.test/User/Register/RegisterValidatorUserTest.cs
@@ -0,0 +1,141 @@
+﻿using FluentAssertions;
+using MeuLivroReceitas.Application.UseCases.User.Register;
+using MeuLivroReceitas.Comunication.Request;
+using MeuLivroReceitas.Exceptions;
+using UtilsTests.Requests;
+using Xunit;
+
+namespace Validators.test.User.Register;
+
+public class RegisterValidatorUserTest
+{
+    //cada funcao fara um test
+
+    [Fact]//informa que é um test
+    public void ValidateSuccess()
+    {
+        var validator = new RegisterUserValidator();
+
+        var req = RequestRegisterUserBuilder.Construct();
+
+        //valida requisicao e retorna o resultado
+        var result = validator.Validate(req);
+
+        //Should vem do pacote fluentAssertions
+        result.IsValid.Should().BeTrue();
+    }    
+    
+    [Fact]//informa que é um test
+    public void ValidateErroEmptyName()
+    {
+        var validator = new RegisterUserValidator();
+
+        var req = RequestRegisterUserBuilder.Construct();
+        req.Name = string.Empty;
+
+        //valida requisicao e retorna o resultado
+        var result = validator.Validate(req);
+
+        //Should vem do pacote fluentAssertions
+        result.IsValid.Should().BeFalse();
+        result
+            .Errors.Should()
+            .ContainSingle()
+            .And.Contain(err => err.ErrorMessage.Equals(ResourceMessageError.EMPTY_USER));
+    }
+
+    [Fact]//informa que é um test
+    public void ValidateErroEmptyEmail()
+    {
+        var validator = new RegisterUserValidator();
+
+        var req = RequestRegisterUserBuilder.Construct();
+        req.Email = string.Empty;
+
+        //valida requisicao e retorna o resultado
+        var result = validator.Validate(req);
+
+        //Should vem do pacote fluentAssertions
+        result.IsValid.Should().BeFalse();
+        result
+            .Errors.Should()
+            .ContainSingle()
+            .And.Contain(err => err.ErrorMessage.Equals(ResourceMessageError.EMAIL_EMPTY));
+    }
+
+    [Fact]//informa que é um test
+    public void ValidateErroEmptyPassword()
+    {
+        var validator = new RegisterUserValidator();
+
+        var req = RequestRegisterUserBuilder.Construct();
+        req.Password = string.Empty;
+
+        //valida requisicao e retorna o resultado
+        var result = validator.Validate(req);
+
+        //Should vem do pacote fluentAssertions
+        result.IsValid.Should().BeFalse();
+        result
+            .Errors.Should()
+            .ContainSingle()
+            .And.Contain(err => err.ErrorMessage.Equals(ResourceMessageError.PASSWORD_EMPTY));
+    }
+
+    [Fact]//informa que é um test
+    public void ValidateErroEmptyPhone()
+    {
+        var validator = new RegisterUserValidator();
+
+        var req = RequestRegisterUserBuilder.Construct();
+        req.Phone = string.Empty;
+
+        //valida requisicao e retorna o resultado
+        var result = validator.Validate(req);
+
+        //Should vem do pacote fluentAssertions
+        result.IsValid.Should().BeFalse();
+        result
+            .Errors.Should()
+            .ContainSingle()
+            .And.Contain(err => err.ErrorMessage.Equals(ResourceMessageError.PHONE_EMPTY));
+    }
+
+    [Fact]//informa que é um test
+    public void ValidateErroShortPassword()
+    {
+        var validator = new RegisterUserValidator();
+
+        var req = RequestRegisterUserBuilder.Construct();
+        req.Password = "1234";
+
+        //valida requisicao e retorna o resultado
+        var result = validator.Validate(req);
+
+        //Should vem do pacote fluentAssertions
+        result.IsValid.Should().BeFalse();
+        result
+            .Errors.Should()
+            .ContainSingle()
+            .And.Contain(err => err.ErrorMessage.Equals(ResourceMessageError.PASSWORD_MIN_LENGTH));
+    }
+
+    [Fact]//informa que é um test
+    public void ValidateErroInvalidEmail()
+    {
+        var validator = new RegisterUserValidator();
+
+        var req = RequestRegisterUserBuilder.Construct();
+        req.Email = "email";
+
+        //valida requisicao e retorna o resultado
+        var result = validator.Validate(req);
+
+        //Should vem do pacote fluentAssertions
+        result.IsValid.Should().BeFalse();
+        result
+            .Errors.Should()
+            .ContainSingle()
+            .And.Contain(err => err.ErrorMessage.Equals(ResourceMessageError.EMAIL_INVALID));
+    }
+}
