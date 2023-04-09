d80504a84884d618a2b0390ea1bf1ea8a7458126
Ruhan Correa Soares <ruhan_cs@hotmail.com>
2023-04-09 12:57:31 -0300

    adicionado teste de unidade do endpoint regitrar usuario

---
+++ tests/WebApi.Test/V1/ControllerBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Text;
+using MeuLivroReceitas.Exceptions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace WebApi.Test.V1;
+
+//MeuLivroReceitasWebApplicationFactory criada recebe o parametro o Program
+//criado uma definicao de classe para program
+public class ControllerBase : IClassFixture<MeuLivroReceitasWebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public ControllerBase(MeuLivroReceitasWebApplicationFactory<Program> factory)
+    {
+        //inicializar o httpclient
+        _client = factory.CreateClient();
+        //para ResourceMessageError ser na mesma linguagem que a aplicacao 
+        ResourceMessageError.Culture = CultureInfo.CurrentCulture;
+    }
+
+    protected async Task<HttpResponseMessage> PostRequest(string method, object body)
+    {
+        //corpo do body tratado para utilizar o postAsync
+        var jsonString = JsonConvert.SerializeObject(body);
+
+        return await _client.PostAsync(method, new StringContent(jsonString, Encoding.UTF8, "application/json"));
+    }
+}
