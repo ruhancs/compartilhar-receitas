bc05513dfc6f5042cbc5121b0b247dc648e30fec
Ruhan Correa Soares <ruhan_cs@hotmail.com>
2023-04-10 20:13:37 -0300

    adicionado teste de unidade atualizar senha

---
+++ tests/UtilsTests/Requests/RequestUpdatePasswordBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Bogus;
+using MeuLivroReceitas.Application.UseCases.User.Register;
+using MeuLivroReceitas.Application.UseCases.User.Update;
+using MeuLivroReceitas.Comunication.Request;
+
+namespace UtilsTests.Requests;
+
+public class RequestUpdatePasswordBuilder
+{
+    public static RequestUpdatePasswordJson Construct(int passwordLenght=10 )
+    {
+        return new Faker<RequestUpdatePasswordJson>()
+            .RuleFor(c => c.Password, f => f.Internet.Password(10))
+            .RuleFor(c => c.NewPassword, f => f.Internet.Password(passwordLenght));
+
+    }
+
+
+}
---
+++ tests/Validators.test/User/UpdatePassword/UpdatePasswordValidatorTest.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using FluentValidation;
+using MeuLivroReceitas.Application.UseCases.User.Register;
+using MeuLivroReceitas.Application.UseCases.User.Update;
+using MeuLivroReceitas.Exceptions;
+using UtilsTests.Requests;
+using Xunit;
+
+namespace Validators.test.User.UpdatePassword;
+
+public class UpdatePasswordValidatorTest
+{
+    [Fact]//informa que é um test
+    public void ValidateSuccess()
+    {
+        var validator = new UpdatePasswordValidator();
+
+        var req = RequestUpdatePasswordBuilder.Construct();
+
+        //valida requisicao e retorna o resultado
+        var result = validator.Validate(req);
+
+        //Should vem do pacote fluentAssertions
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]//informa que é um test
+    public void ValidateErroEmptyPassword()
+    {
+        var validator = new UpdatePasswordValidator();
+
+        var req = RequestUpdatePasswordBuilder.Construct();
+        req.NewPassword = string.Empty;
+
+        //valida requisicao e retorna o resultado
+        var result = validator.Validate(req);
+
+        //Should vem do pacote fluentAssertions
+        result.IsValid.Should().BeFalse();
+        result
+            .Errors.Should()
+            .ContainSingle()
+            .And.Contain(err => err.ErrorMessage.Equals(ResourceMessageError.PASSWORD_EMPTY));
+    }
+}
