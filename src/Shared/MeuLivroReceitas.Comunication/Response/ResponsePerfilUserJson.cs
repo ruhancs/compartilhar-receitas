644fe0cc1e9e9c165f80b7fd7c5a9d46a2f1bf6d
Ruhan Correa Soares <ruhan_cs@hotmail.com>
2023-04-13 18:20:50 -0300

    feat: adicionado recuperar perfil usuario

---
+++ src/Shared/MeuLivroReceitas.Comunication/Response/ResponsePerfilUserJson.cs
@@ -0,0 +1,8 @@
+﻿namespace MeuLivroReceitas.Comunication.Response;
+
+public class ResponsePerfilUserJson
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+}
